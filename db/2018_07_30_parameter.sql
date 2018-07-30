f39a575d244e4ac20eb6f0cdab0e2e1d5fa28de7
epiii <gue.elfrianto@gmail.com>
2018-07-30 12:10:03 +0700

    db - paramter - add some data:promote & profile

---
+++ db/2018_07_30_parameter.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 30, 2018 at 12:05 PM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.28-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sf_db019`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parameter`
+--
+
+CREATE TABLE `parameter` (
+  `id_param` int(11) NOT NULL,
+  `nama` text NOT NULL,
+  `param1` text,
+  `param2` text,
+  `param3` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `parameter`
+--
+
+INSERT INTO `parameter` (`id_param`, `nama`, `param1`, `param2`, `param3`) VALUES
+(26, 'frame3.png', 'frame', 'keterangan gambar frame 1', ''),
+(25, 'frame2.png', 'frame', 'keterangan gambar frame 2', ''),
+(24, 'frame1.png', 'frame', 'keterangan gambar frame 1', ''),
+(23, 'SF3.png', 'promote', 'keterangan gambar promote 3', '250,650,120,120,10,720'),
+(22, 'SF2.png', 'promote', 'keterangan gambar promote 2', '490,530,120,120,460,680'),
+(21, 'SF1.png', 'promote', 'keterangan gambar promote 1', '490,330,120,120,330,400');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `parameter`
+--
+ALTER TABLE `parameter`
+  ADD PRIMARY KEY (`id_param`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `parameter`
+--
+ALTER TABLE `parameter`
+  MODIFY `id_param` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
